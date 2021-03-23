8c003a5f0b90994bfa8e37950f64670ae6ba4f74
Mikk Martin <mikk.martin@gmail.com>
2021-03-23 13:08:48 +0000

    initial connections

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,12 @@ export default function Home() {
     { type: 'number', id: '1', inputs: [5] },
     { type: 'number', id: '2' },
     { type: 'add', x: 160, y: 20, id: '3', inputs: [0, 0, 2] },
-    { type: 'number', x: 280, y: 20, id: '4' },
-    { type: 'add' },
+    { type: 'add', x: 270, y: 20, id: '4' },
+    { type: 'number', x: 380, y: 20, id: '5' },
     { source: '1', target: '3' },
     { source: '2', target: '3' },
     { source: '3', target: '4' },
+    { source: '4', target: '5' },
   ]
   return (
     <>
